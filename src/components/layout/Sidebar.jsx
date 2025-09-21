df57243c0e0424c4ce62a9bf3074fe586122850c
Satyam Agarwal <sat7ag@ibm.com>
2025-09-21 21:23:56 +0530

    made logo clickable

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { FiHome, FiBox, FiUsers, FiLayout, FiBriefcase, FiX  } from 'react-icons/fi';
+import { FiHome, FiBox, FiUsers, FiLayout, FiBriefcase, FiX } from 'react-icons/fi';
 import { BiSolidUserAccount } from "react-icons/bi";
 import { IoMdBook } from "react-icons/io";
-import { MdOutlineCorporateFare, MdPostAdd  } from "react-icons/md";
-import DatalytixLogoLight  from '../../assets/images/logo.png';
-import DatalytixLogoDark  from '../../assets/images/logo-dark.png';
+import { MdOutlineCorporateFare, MdPostAdd } from "react-icons/md";
+import DatalytixLogoLight from '../../assets/images/logo.png';
+import DatalytixLogoDark from '../../assets/images/logo-dark.png';
 import { useTheme } from '../../hooks/useTheme';
 
 const Sidebar = ({ isSidebarOpen, setSidebarOpen }) => {
@@ -21,31 +21,33 @@ const Sidebar = ({ isSidebarOpen, setSidebarOpen }) => {
       ></div>
 
       <aside className={`bg-card border-r border-border p-5 flex-shrink-0 w-64 z-40 fixed lg:relative lg:translate-x-0 h-full transition-transform transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-        <div className="flex justify-between items-center mb-8">  
-          <img 
-            src={theme === 'light' ? DatalytixLogoLight : DatalytixLogoDark} 
-            alt="Datalytix Logo" 
-            className="w-64" 
-          />
+        <div className="flex justify-between items-center mb-8">
+          <NavLink to="/dashboards/default">
+            <img
+              src={theme === 'light' ? DatalytixLogoLight : DatalytixLogoDark}
+              alt="Datalytix Logo"
+              className="w-64"
+            />
+          </NavLink>
           <button onClick={() => setSidebarOpen(false)} className="lg:hidden text-muted">
             <FiX size={24} />
           </button>
         </div>
         <nav className="space-y-6">
           <NavSection title="Favorites">
-            <NavLink to="/overview" className={({isActive}) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><FiHome /> <span className="ml-3">Overview</span></NavLink>
-            <NavLink to="/projects" className={({isActive}) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><FiBriefcase /> <span className="ml-3">Projects</span></NavLink>
+            <NavLink to="/overview" className={({ isActive }) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><FiHome /> <span className="ml-3">Overview</span></NavLink>
+            <NavLink to="/projects" className={({ isActive }) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><FiBriefcase /> <span className="ml-3">Projects</span></NavLink>
           </NavSection>
           <NavSection title="Dashboards">
-            <NavLink to="/dashboards/default" className={({isActive}) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><FiLayout /> <span className="ml-3">Default</span></NavLink>
-            <NavLink to="/orders" className={({isActive}) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><FiBox /> <span className="ml-3">Order List</span></NavLink>
-            <NavLink to="/online-courses" className={({isActive}) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><IoMdBook /> <span className="ml-3">Online Courses</span></NavLink>
+            <NavLink to="/dashboards/default" className={({ isActive }) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><FiLayout /> <span className="ml-3">Default</span></NavLink>
+            <NavLink to="/orders" className={({ isActive }) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><FiBox /> <span className="ml-3">Order List</span></NavLink>
+            <NavLink to="/online-courses" className={({ isActive }) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><IoMdBook /> <span className="ml-3">Online Courses</span></NavLink>
           </NavSection>
-           <NavSection title="Pages">
-            <NavLink to="/user-profile" className={({isActive}) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><FiUsers /> <span className="ml-3">User Profile</span></NavLink>
-            <NavLink to="/account" className={({isActive}) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><BiSolidUserAccount /> <span className="ml-3">Account</span></NavLink>
-            <NavLink to="/corporate" className={({isActive}) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><MdOutlineCorporateFare  /> <span className="ml-3">Corporate</span></NavLink>
-            <NavLink to="/blog" className={({isActive}) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><MdPostAdd /> <span className="ml-3">Blog</span></NavLink>
+          <NavSection title="Pages">
+            <NavLink to="/user-profile" className={({ isActive }) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><FiUsers /> <span className="ml-3">User Profile</span></NavLink>
+            <NavLink to="/account" className={({ isActive }) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><BiSolidUserAccount /> <span className="ml-3">Account</span></NavLink>
+            <NavLink to="/corporate" className={({ isActive }) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><MdOutlineCorporateFare /> <span className="ml-3">Corporate</span></NavLink>
+            <NavLink to="/blog" className={({ isActive }) => `${navLinkClasses} ${isActive ? activeLinkClasses : ''}`}><MdPostAdd /> <span className="ml-3">Blog</span></NavLink>
           </NavSection>
         </nav>
       </aside>
